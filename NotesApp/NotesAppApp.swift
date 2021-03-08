e64e64a6d8a3749011028441105350cf2c6730bc
Eric Sims <e.sims@icloud.com>
2021-03-07 17:36:48 -0700

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Eric Sims on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
